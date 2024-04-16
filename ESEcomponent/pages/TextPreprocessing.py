25e251c33e91946052567b1e0122c25f01f6495d
Alankritha S <109807524+AlankrithaS@users.noreply.github.com>
2024-04-16 15:14:29 +0530

    Update TextPreprocessing.py

--- ESEcomponent/pages/TextPreprocessing.py
+++ ESEcomponent/pages/TextPreprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from wordcloud import WordCloud
+
 import matplotlib.pyplot as plt
 
 nltk.download('stopwords')
